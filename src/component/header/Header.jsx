bbd59066b39a07d08f91d23ec9b6fa4fe6ace519
kevin <rfahedkevin@gmail.com>
2022-07-06 14:49:29 +0400

    header 1.0

--- src/component/header/Header.jsx
+++ src/component/header/Header.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
 import "./header.css"
+import CTA from './Cta';
+import ME from '../../assets/Me.jpg'
+import Social from './Social';
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5> Hello I'm </h5>
+        <h1> Rakotoarsion Kevin </h1>
+        <h5 className="text-light">
+            Developer
+        </h5>
+        <CTA/>
+        <Social/>
+        <div className="me">
+          <img src={ME} alt="me"/>
+        </div>
+        <a href="#contact" className='scroll'>Scroll</a>
+      </div>
+    </header>
   )
 }
 
